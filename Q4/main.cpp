43765af1efd07bc4450efa57df1943255bc49f5a
Sam Grumley <grumsamu@gmail.com>
2020-04-23 00:57:44 +1000

    Outline completed

---
+++ Q4/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    // unordered between 0 -> n
+    // no dupes -> set
+    // O(1) add, delete, search
+    // O(n) for clearing list
+
+    // Maybe linked list index to or hash function to number
+
+    return 1;
+}
